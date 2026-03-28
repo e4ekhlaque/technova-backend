5513e9722a5122a48fc195e7b72d5d822fea2da2
Mohammad Ekhlaque <ekhlaquemd48@outlook.com>
2026-03-29 01:47:15 +0530

    updated server.js

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://technova.vercel.app",
+  }),
+);
 app.use(express.json());
 
 // Routes
