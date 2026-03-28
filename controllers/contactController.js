3337301a09fc02afc642712d335fbaff19eb5e9b
Mohammad Ekhlaque <ekhlaquemd48@outlook.com>
2026-03-29 01:04:35 +0530

    Implementing Backend

---
+++ controllers/contactController.js
@@ -0,0 +1,14 @@
+import Contact from "../models/contact.js";
+
+export const submitContact = async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+
+    const newContact = new Contact({ name, email, message });
+    await newContact.save();
+
+    res.status(201).json({ message: "Message saved successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import contactRoutes from "./routes/contactRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api", contactRoutes);
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
